14eaa179a939223723203aa1357ff1f5a9acb587
lcs-dsmith <dsmith@lcs.on.ca>
2021-02-25 10:00:15 -0500

    Text now fires into the atmosphere at mach 2

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct CustomComposableView: View {
     
-    @State private var offset: CGFloat = 40
+    @State private var offset: CGFloat = 0
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
      
         Text("VROOOOM")
-            .offset(x: offset, y: offset)
-            .onTapGesture {
-                offset = 100
+            .offset(x: 0, y: offset)
+            .onReceive(timer) { _ in
+                withAnimation(.linear(duration: 0.1)) {
+                    offset = -1000
+                }
             }
-            .animation(.interpolatingSpring(stiffness: 50, damping: 0.5))
+//            .animation(.interpolatingSpring(stiffness: 50, damping: 0.5))
             
     }
 }
